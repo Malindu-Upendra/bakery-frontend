2017081bbd73e0f5242eabd5e2225ba0d0ff5e98
Malindu Upendra <upendramalindu116@gmail.com>
2024-04-19 16:36:29 +0530

    Api Connected With Navigation bar

--- src/services/user_service.js
+++ src/services/user_service.js
@@ -2,10 +2,20 @@ import axios from "axios";
 
 var apiUrl = "http://localhost:3500/"
 
+const headers = {
+  Authorization: `Bearer ${localStorage.getItem("token")}`,
+};
+
 export async function registerUser(obj) {
   return await axios.post(apiUrl + 'users', obj).then(data => data);
 }
 
 export async function loginUser(obj) {
   return await axios.post(apiUrl + 'auth', obj).then(data => data);
+}
+
+export async function getLoggedInUser() {
+  return await axios
+    .get(apiUrl + "users/logged-in-user", { headers })
+    .then((data) => data);
 }
