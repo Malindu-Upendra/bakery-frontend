ca1991b350fbba981038e18f2aeab48ab1293532
arosh.segar <arosh.segar@rootcodelabs.com>
2024-04-19 20:09:08 +0530

    fix: edit and add member modals

--- src/components/organisms/users/UsersOrganism.tsx
+++ src/components/organisms/users/UsersOrganism.tsx
@@ -15,6 +15,9 @@ import {
   IconButton,
   Tooltip,
 } from "@material-tailwind/react";
+import { useState } from "react";
+import AddMemberForm from "../../templates/modals/AddMemberForm";
+import EditMemberForm from "../../templates/modals/EditMemberForm copy";
 
 const TABS = [
   {
@@ -82,26 +85,42 @@ const TABLE_ROWS = [
 ];
 
 const UsersOrganism = () => {
+  const [isAddUserModal, setIsAddUserModal] = useState(false);
+  const [isEditUserModal, setIsEditUserModal] = useState(false);
   return (
     <div className="w-full">
-      <Card className="h-full w-full" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
+      <Card
+        className="h-full w-full"
+        placeholder={undefined}
+        onPointerEnterCapture={undefined}
+        onPointerLeaveCapture={undefined}
+      >
         <CardHeader
           floated={false}
           shadow={false}
           className="rounded-none"
-          placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}        >
+          placeholder={undefined}
+          onPointerEnterCapture={undefined}
+          onPointerLeaveCapture={undefined}
+        >
           <div className="mb-8 flex items-center justify-between gap-8">
             <div>
               <Typography
                 variant="h5"
                 color="blue-gray"
-                placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}              >
+                placeholder={undefined}
+                onPointerEnterCapture={undefined}
+                onPointerLeaveCapture={undefined}
+              >
                 Members list
               </Typography>
               <Typography
                 color="gray"
                 className="mt-1 font-normal"
-                placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}              >
+                placeholder={undefined}
+                onPointerEnterCapture={undefined}
+                onPointerLeaveCapture={undefined}
+              >
                 See information about all members
               </Typography>
             </div>
@@ -110,23 +129,40 @@ const UsersOrganism = () => {
                 variant="outlined"
                 color="blue"
                 size="sm"
-                placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}              >
+                placeholder={undefined}
+                onPointerEnterCapture={undefined}
+                onPointerLeaveCapture={undefined}
+              >
                 view all
               </Button>
               <Button
                 className="flex items-center gap-3"
                 size="sm"
                 color="blue"
-                placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}              >
+                placeholder={undefined}
+                onPointerEnterCapture={undefined}
+                onPointerLeaveCapture={undefined}
+                onClick={() => setIsAddUserModal(true)}
+              >
                 <FaUserPlus strokeWidth={2} className="h-4 w-4" /> Add member
               </Button>
             </div>
           </div>
           <div className="flex flex-col items-center justify-between gap-4 md:flex-row">
             <Tabs value="all" className="w-full md:w-max">
-              <TabsHeader placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
+              <TabsHeader
+                placeholder={undefined}
+                onPointerEnterCapture={undefined}
+                onPointerLeaveCapture={undefined}
+              >
                 {TABS.map(({ label, value }) => (
-                  <Tab key={value} value={value} placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
+                  <Tab
+                    key={value}
+                    value={value}
+                    placeholder={undefined}
+                    onPointerEnterCapture={undefined}
+                    onPointerLeaveCapture={undefined}
+                  >
                     &nbsp;&nbsp;{label}&nbsp;&nbsp;
                   </Tab>
                 ))}
@@ -137,11 +173,19 @@ const UsersOrganism = () => {
                 label="Search"
                 icon={<FaSearch className="h-5 w-5" />}
                 crossOrigin={undefined}
-                color="blue" onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}              />
+                color="blue"
+                onPointerEnterCapture={undefined}
+                onPointerLeaveCapture={undefined}
+              />
             </div>
           </div>
         </CardHeader>
-        <CardBody className="overflow-scroll px-0" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
+        <CardBody
+          className="overflow-scroll px-0"
+          placeholder={undefined}
+          onPointerEnterCapture={undefined}
+          onPointerLeaveCapture={undefined}
+        >
           <table className="mt-4 w-full min-w-max table-auto text-left">
             <thead>
               <tr>
@@ -154,7 +198,10 @@ const UsersOrganism = () => {
                       variant="small"
                       color="blue-gray"
                       className="flex items-center justify-between gap-2 font-normal leading-none opacity-70"
-                      placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}                    >
+                      placeholder={undefined}
+                      onPointerEnterCapture={undefined}
+                      onPointerLeaveCapture={undefined}
+                    >
                       {head}{" "}
                       {index !== TABLE_HEAD.length - 1 && (
                         <FaSort strokeWidth={2} className="h-4 w-4" />
@@ -180,20 +227,29 @@ const UsersOrganism = () => {
                             src={img}
                             alt={name}
                             size="sm"
-                            placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}                          />
+                            placeholder={undefined}
+                            onPointerEnterCapture={undefined}
+                            onPointerLeaveCapture={undefined}
+                          />
                           <div className="flex flex-col">
                             <Typography
                               variant="small"
                               color="blue-gray"
                               className="font-normal"
-                              placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}                            >
+                              placeholder={undefined}
+                              onPointerEnterCapture={undefined}
+                              onPointerLeaveCapture={undefined}
+                            >
                               {name}
                             </Typography>
                             <Typography
                               variant="small"
                               color="blue-gray"
                               className="font-normal opacity-70"
-                              placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}                            >
+                              placeholder={undefined}
+                              onPointerEnterCapture={undefined}
+                              onPointerLeaveCapture={undefined}
+                            >
                               {email}
                             </Typography>
                           </div>
@@ -205,14 +261,20 @@ const UsersOrganism = () => {
                             variant="small"
                             color="blue-gray"
                             className="font-normal"
-                            placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}                          >
+                            placeholder={undefined}
+                            onPointerEnterCapture={undefined}
+                            onPointerLeaveCapture={undefined}
+                          >
                             {job}
                           </Typography>
                           <Typography
                             variant="small"
                             color="blue-gray"
                             className="font-normal opacity-70"
-                            placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}                          >
+                            placeholder={undefined}
+                            onPointerEnterCapture={undefined}
+                            onPointerLeaveCapture={undefined}
+                          >
                             {org}
                           </Typography>
                         </div>
@@ -232,13 +294,22 @@ const UsersOrganism = () => {
                           variant="small"
                           color="blue-gray"
                           className="font-normal"
-                          placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}                        >
+                          placeholder={undefined}
+                          onPointerEnterCapture={undefined}
+                          onPointerLeaveCapture={undefined}
+                        >
                           {date}
                         </Typography>
                       </td>
                       <td className={classes}>
                         <Tooltip content="Edit User">
-                          <IconButton variant="text" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
+                          <IconButton
+                            variant="text"
+                            onClick={() => setIsEditUserModal(true)}
+                            placeholder={undefined}
+                            onPointerEnterCapture={undefined}
+                            onPointerLeaveCapture={undefined}
+                          >
                             <FaPencilAlt className="h-4 w-4" />
                           </IconButton>
                         </Tooltip>
@@ -252,12 +323,18 @@ const UsersOrganism = () => {
         </CardBody>
         <CardFooter
           className="flex items-center justify-between border-t border-blue-gray-50 p-4"
-          placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}        >
+          placeholder={undefined}
+          onPointerEnterCapture={undefined}
+          onPointerLeaveCapture={undefined}
+        >
           <Typography
             variant="small"
             color="blue-gray"
             className="font-normal"
-            placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}          >
+            placeholder={undefined}
+            onPointerEnterCapture={undefined}
+            onPointerLeaveCapture={undefined}
+          >
             Page 1 of 10
           </Typography>
           <div className="flex gap-2">
@@ -265,19 +342,27 @@ const UsersOrganism = () => {
               color="blue"
               variant="outlined"
               size="sm"
-              placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}            >
+              placeholder={undefined}
+              onPointerEnterCapture={undefined}
+              onPointerLeaveCapture={undefined}
+            >
               Previous
             </Button>
             <Button
               color="blue"
               variant="outlined"
               size="sm"
-              placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}            >
+              placeholder={undefined}
+              onPointerEnterCapture={undefined}
+              onPointerLeaveCapture={undefined}
+            >
               Next
             </Button>
           </div>
         </CardFooter>
       </Card>
+      <AddMemberForm open={isAddUserModal} setOpen={setIsAddUserModal} />
+      <EditMemberForm open={isEditUserModal} setOpen={setIsEditUserModal} />
     </div>
   );
 };
